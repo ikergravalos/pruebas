4d18ffcf613a2cc7185daa0e1268ff7e8fb7cab3
Iker Martinez Gomez <iker.martinez.gomez@hotmail.com>
2015-03-27 21:22:29 +0100

    Carga inicial del proyecto

---
+++ src/ejemplogit/EjemploGit.java
@@ -0,0 +1,13 @@
+package ejemplogit;
+
+
+public class EjemploGit {
+
+    
+    public static void main(String[] args) {
+        
+        System.out.println("Quiero vacaciones!!!");
+        
+    }
+
+}
